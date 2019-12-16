e20df67ffab7524eb83a4df64ee28b96bd7c9cce
Nathaniel Schweinberg <natanielks@gmail.com>
2019-12-16 17:33:39 -0600

    add function code and initial npm package

---
+++ index.js
@@ -0,0 +1,84 @@
+module.exports = {
+  getParameters,
+  errorOnMissingVars,
+  getConfig
+}
+
+async function getParameters( env, required_params, ssm ) {
+
+	// Detect which parameters already have environment variables defined and
+	// reduce to the parameters which still need values populated. This is used
+	// for local environments, such as DB_PASS already being defined and not
+	// needing a parameter retrieved.
+	const params = required_params.reduce( ( acc, curr ) => {
+		if ( ! env[curr.replace( 'PARAM_', '' )] ) {
+			acc.push( curr );
+		}
+		return acc;
+	}, [] );
+
+	if ( params.length === 0 ) {
+		return env;
+	}
+
+	// Error if parameters are missing
+	errorOnMissingVars( params, env );
+
+	// object keyed on parameter name for reverse lookup when we want to hydrate
+	// the environment variable without the PARAM_ prefix later
+	const paramsMapReverse = params.reduce( ( acc, curr ) => {
+		acc[env[curr]] = curr;
+		return acc;
+	}, {} );
+
+	// Names of SSM Parameters to retrieve
+	const paramNames = params.map( param => env[param] );
+
+	// Get the parameters
+	const values = await ssm
+		.getParameters( {
+			Names: paramNames,
+			WithDecryption: true,
+		} )
+		.promise();
+
+	// Error if parameters weren't found
+	if ( values.InvalidParameters.length > 0 ) {
+		throw new Error(
+			'Failed retrieving SSM parameters: ' + values.InvalidParameters.join( ', ' )
+		);
+	}
+
+	// Hydrate the env vars without the PARAM_ prefix
+	const compiled = values.Parameters.reduce( ( acc, curr ) => {
+		acc[paramsMapReverse[curr.Name].replace( 'PARAM_', '' )] = curr.Value;
+		return acc;
+	}, env );
+
+	return compiled;
+}
+
+function errorOnMissingVars( env, vars ) {
+	let missingVars = [];
+	vars.forEach( key => {
+		if ( ! env[key] ) {
+			missingVars.push( key );
+		}
+	} );
+
+	if ( missingVars.length > 0 ) {
+		throw new Error( 'Required variables missing: ' + missingVars.join( ', ' ) );
+	}
+}
+
+function getConfig( env, vars ) {
+
+	errorOnMissingVars( env, vars );
+
+	const config = vars.reduce( ( acc, curr ) => {
+		acc[curr] = env[curr];
+		return acc;
+	}, {} );
+
+	return config;
+}
